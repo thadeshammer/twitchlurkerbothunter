666788bd246c2876dc4b02d19972fa13eaf462ad
thades <thadeshammer@gmail.com>
2024-06-25 11:41:21 -0400

    REQUIRES TESTING OF TABLE CREATION

--- app/db/base.py
+++ app/db/base.py
@@ -1,12 +1,27 @@
-from sqlalchemy import create_engine
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_scoped_session,
+    create_async_engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://user:password@db/mydatabase"
+DATABASE_URL = "mysql+aiomysql://user:password@db/mydatabase"
 
-# Defining these here instead of db.py will fix circular imports. Hopefully.
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Defining these here instead of db.py addresses circular imports. Hopefully.
+engine = create_async_engine(DATABASE_URL, future=True)
+async_session_factory = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+AsyncSessionLocal = async_scoped_session(async_session_factory, scopefunc=current_task)
+
 
 # Create a base class for all declarative models to inherit from.
 Base = declarative_base()
+
+
+async def async_create_all_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- app/db/db.py
+++ app/db/db.py
@@ -1,32 +1,56 @@
 # app/db.py
-import time
-from contextlib import contextmanager
-from typing import Generator, Optional
+"""
+    SQLAlchemy DB Session Helper Functions
+
+    Sample:
+
+    # Assuming you have a model defined, for example:
+    # from your models import YourModel
+
+        async def async_main():
+            # Write transaction
+            async with get_db() as db:
+                new_record = YourModel(field1="value1", field2="value2")
+                db.add(new_record)
+                await db.commit()
+
+            # Read transaction
+            async with get_db() as db:
+                result = await db.execute(select(YourModel).filter_by(field1="value1"))
+                record = result.scalars().first()
+                print(record)
+
+        if __name__ == "__main__":
+            asyncio.run(async_main())
+"""
+import asyncio
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Optional
 
 from flask import current_app
 from sqlalchemy.exc import OperationalError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import ConfigKey
 from app.exceptions import DatabaseConnectionError
 
-from .base import SessionLocal
+from .base import AsyncSessionLocal
 
 
-def _get_session() -> Session:
+async def _get_session() -> AsyncSession:
     retries = 0
     max_retries = current_app.config[ConfigKey.SQLALCHEMY_CONNECT_MAX_RETRIES]
     retry_interval = current_app.config[ConfigKey.SQLALCHEMY_CONNECT_RETRY_INTERVAL]
 
-    db: Optional[Session] = None
+    db: Optional[AsyncSession] = None
     while retries < max_retries and db is None:
         try:
 
-            db = SessionLocal()
+            db = AsyncSessionLocal()
 
         except OperationalError as e:
             retries += 1
-            time.sleep(retry_interval)
+            await asyncio.sleep(retry_interval)
             if retries >= max_retries:
                 raise DatabaseConnectionError(
                     "Failed to connect to the database after multiple retries."
@@ -36,10 +60,11 @@ def _get_session() -> Session:
     return db
 
 
-@contextmanager
-def get_db() -> Generator[Session, None, None]:
-    db = _get_session()
+@asynccontextmanager
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    db = await _get_session()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
+        await AsyncSessionLocal.remove()
