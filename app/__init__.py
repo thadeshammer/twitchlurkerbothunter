f6442dd0648b65dcd46fd1bb8aebf1a4b6e18c8d
thades <thadeshammer@gmail.com>
2024-07-10 23:00:22 -0400

    switching to uvicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 # app/__init__.py
 import asyncio
-import atexit
-import logging.config
+import logging
 import os
 
 from flask import Flask
@@ -9,30 +8,16 @@ from flask import Flask
 from app.config import Config
 
 # from app.db import async_create_all_tables
-# from app.models import (
-#     scanning_session,
-#     secrets,
-#     stream_categories,
-#     stream_viewerlist_fetch,
-#     suspected_bot,
-#     twitch_user_data,
-#     viewer_sighting,
-# )
+from app.models import (
+    scanning_session,
+    secrets,
+    stream_categories,
+    stream_viewerlist_fetch,
+    suspected_bot,
+    twitch_user_data,
+    viewer_sighting,
+)
 from app.routes import register_routes
-from app.util import setup_logging
-
-# Logger setup outside of create_app
-setup_logging(Config.LOGGING_CONFIG_FILE)
-logging.getLogger("asyncio").setLevel(
-    logging.WARNING
-)  #  "Using selector: EpollSelector" spam
-logger = logging.getLogger("app")
-logger.info("Logger is ready.")
-
-
-def _at_shutdown() -> None:
-    """Handle any shutdown specifics for Flask."""
-    logger.info("Server is shutting down.")
 
 
 def create_app() -> Flask:
@@ -42,11 +27,12 @@ def create_app() -> Flask:
     app.config.from_object(Config)
     app.debug = False
 
-    atexit.register(_at_shutdown)
+    logger = logging.getLogger("app")
+    logger.info("Logger is ready.")
 
     # Log the Gunicorn worker PID
     worker_pid = os.getpid()
-    logger.info(f"Gunicorn worker PID: {worker_pid}")
+    logger.info(f"Worker PID: {worker_pid}")
 
     with app.app_context():
         register_routes(app)
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import logging.config
 import os
 import ssl
 
 from app import create_app
+from app.config import Config
+from app.util import setup_logging
 
 CERT_FILE_PATH = "/secrets/cert.pem"
 KEY_FILE_PATH = "/secrets/key.pem"
@@ -10,7 +13,22 @@ CERT_PASSKEY = str(os.getenv("CERT_PASSKEY"))
 if not CERT_PASSKEY:
     raise EnvironmentError("CERT_PASSKEY environment variable not set")
 
+# Logger setup outside of create_app
+setup_logging(Config.LOGGING_CONFIG_FILE)
+logging.getLogger("asyncio").setLevel(
+    logging.WARNING
+)  #  "Using selector: EpollSelector" spam
+logger = logging.getLogger("app")
+logger.info("Logger is ready.")
+
 app = create_app()
+
+
+@app.teardown_appcontext
+def shutdown_event(exception=None):
+    logger.info("Server is shutting down...")
+    if exception is not None:
+        logger.error(f"Shutdown error: {exception}")
 
 
 if __name__ == "__main__":
